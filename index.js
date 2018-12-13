56b38eae534b68b897ce45432f2a2b1cd06426f3
evolfo <spampinatodesign@gmail.com>
2018-12-13 12:05:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+	return document.querySelector(selector);
+}
 
+function nestedTarget() {
+	return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+	const ranked = document.getElementsByClassName('ranked-list');
+	for (var i = 0; i < ranked.length; i++) {
+		ranked[i].innerHTML = (i + 1);
+		parseInt(ranked);
+	}
+}
+
+function deepestChild() {
+	return document.querySelector('#grand-node div div div div');
+}
